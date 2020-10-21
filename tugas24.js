bb315c8823cca28a5c29e63d56f0cad2a826743b
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-10-21 11:41:26 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,8 @@
+function panggilSort(){
+  var angka =[32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+  console.log("Sebelumnya : ",angka.toString());
+  console.log("Ascending : ",angka.sort().toString());
+  console.log("Descending : ",angka.reverse().toString());
+}
+panggilSort();
